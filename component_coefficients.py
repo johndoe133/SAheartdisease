be66b06725f1de8a3849e7b8aed065f91b451777
MortenBjerre <48279971+MortenBjerre@users.noreply.github.com>
2019-09-29 15:01:53 +0200

    figure

--- component_coefficients.py
+++ component_coefficients.py
@@ -23,6 +23,7 @@ legendStrs = ['PC'+str(e+1) for e in pcs]
 c = ['r','g','b']
 bw = .2
 r = np.arange(1,M+1)
+plt.figure(figsize=(7,5))
 for i in pcs:    
     plt.bar(r+i*bw, V[:,i], width=bw)
 plt.xticks(r+bw, attributeNames, rotation = 30)
@@ -31,4 +32,6 @@ plt.ylabel('Component coefficients')
 plt.legend(legendStrs)
 plt.grid()
 plt.title('PCA Component Coefficients')
+plt.tight_layout()
+plt.savefig('PCA_Component_Coefficients')
 plt.show()
